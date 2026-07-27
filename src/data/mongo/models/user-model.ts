ce028f531908cda593cb5d5e72d64d7e88ea8e26
juantr94 <hart13ful@gmail.com>
2026-07-27 16:30:48 -0500

    Adds new Property

--- src/data/mongo/models/user-model.ts
+++ src/data/mongo/models/user-model.ts
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Email required'],
         unique: true,
     },
+    emailValidated: {
+        type: Boolean,
+        default: false,
+    },
     img: {
         type: String,
     },
