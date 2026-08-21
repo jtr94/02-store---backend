8768b60a92a700dfc3cdd7050961ba871ba66646
juantr94 <hart13ful@gmail.com>
2026-08-21 17:05:30 -0500

    Implements auth services in auth controller

--- src/presentation/Auth/controller.ts
+++ src/presentation/Auth/controller.ts
@@ -2,26 +2,31 @@ import type { Request, Response } from "express";
 import { checkSchema, validationResult } from "express-validator";
 import { registerSchema } from "../../domain/validator/auth/register.validator";
 import { RegisterDTO } from "../../domain";
+import type { AuthService } from "../services/auth.services";
 
 
 export class AuthController {
     
-    constructor(){} 
+    constructor(
+        public readonly authService : AuthService
+    ){} 
     
-    public login (req: Request, res: Response) {        
+    public login = (req: Request, res: Response) => {        
         res.json("Login");
     }
 
-    public register (req: Request, res: Response) {
-        const result = validationResult(req.body);
-        if (result.isEmpty()) {
-            const registerDTO = RegisterDTO.create(req.body);
-            return res.json("register");
+    public register = (req: Request, res: Response) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
         }
-        res.status(401).json({ errors: result.array() });
+        const registerDTO = RegisterDTO.create(req.body);
+        this.authService.registerUser(registerDTO!)
+          .then(user => res.json(user))
+          .catch(error => { res.status(400).json({error: error.message})});
     }
 
-    public verifyEmail (req: Request, res: Response) {
+    public verifyEmail = (req: Request, res: Response) => {
         res.json("verifyEmail");
     }
 
