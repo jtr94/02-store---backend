dd26e0a2c43f04121aa96bce692fae00661e71f0
juantr94 <hart13ful@gmail.com>
2026-08-21 17:04:55 -0500

    Creates Auth Service

--- src/presentation/services/auth.services.ts
+++ src/presentation/services/auth.services.ts
@@ -1,11 +1,21 @@
 import { UserModel } from "../../data";
-import type { RegisterDTO } from "../../domain";
+import { RegisterDTO } from "../../domain";
 
 export class AuthService{
     constructor(){}
 
     public async registerUser( regdto: RegisterDTO){
-        const exists = await UserModel.findOne({email: regdto.email });
-        if ( exists ) throw new Error('Email already exists!')
+        const exists = await UserModel.findOne({email: regdto.email });        
+        if ( exists ) throw new Error('Email already exists!');
+    
+    try {
+      const user = new UserModel(regdto);
+      await user.save();      
+      return user;
+        
+    } catch (error) {
+        console.log(error);        
+        throw new Error(`${error}`);       
+    }
     }
 }
