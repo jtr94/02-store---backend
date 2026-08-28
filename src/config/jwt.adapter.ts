6eed99b244c575c77b9b233dfba66f874bdfcad7
juantr94 <hart13ful@gmail.com>
2026-08-28 16:56:13 -0500

    Resolves typing error at duration parameter

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -4,10 +4,10 @@ import { envs } from "./envs"
 const JWT_SEED = envs.JWT_SEEDER;
 
 export const jwtAdapter = {
-    async generateToken(payload: any, duration:string = "1h")
+    async generateToken(payload: any, duration: any = "1h")
     {   
         return new Promise((resolve) =>{
-            jwt.sign(payload , JWT_SEED,{expiresIn: duration},  function (err:any, token:any){
+            jwt.sign(payload , JWT_SEED,{ expiresIn: duration },  function (err:any, token:any){
                if (err) return resolve(null);
                resolve(token);          
            })
